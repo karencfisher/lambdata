fe3eb412286d70b4a42c022e56836a9d84013d16
karencfisher <karen-fisher@lambdastudents.com>
2020-07-10 15:02:53 -0600

    refactored as oop

--- statools/tools.py
+++ statools/tools.py
@@ -1,159 +1,232 @@
-import numpy as np
 import pandas as pd
-from scipy.stats import chi2
-
-def mvs(data):
-    '''
-    Calculate mean, variance, and standard deviation for an array.
-
-    input: the array
-    output: dictionary with mean, variation, and standard deviation
-    '''
-
-    # size of array and mean
-    n = len(data)
-    mean = sum(data) / n
-
-    # variance and standard deviation
-    sq_differences = [(value - mean)**2 for value in data]
-    variance = sum(sq_differences) / (n - 1)
-    std = variance ** 0.5
-
-    # package up the results
-    results = {'mean': mean, 'variance': variance, 'standard_deviation': std}
-    return results
 
 
+class DataSet():
+    def __init__(self, data, labels=None):
+        '''
+        Class initialization
 
-def covariance(data, labels=None):
-    '''
-    Calculates variances/covariances between arrays
+        Args:
+        data - 2 dimensional Python array (list). List of lists of
+               values.
+        labels - list of column headings. Optional.
 
-    input: list of arrays
-    output: variance-covariance matrix
-    '''
+        Returns:
+        None
 
-    # Get mean and standard deviations for each array and make parallel list
-    data_mvs = []
-    size = len(data[0])
-    for item in data:
-    # Verify the size of each array is equal to the first array's size
-        assert len(item) == size, f'Array index {data.index(item)} is of different \
-                                    length.'   
-        data_mvs.append(mvs(item))
+        Creates a dictionary in form
+            {column-name: [values]}
 
-    # calculate covariance/variation of each permutation of the two arrays
-    # building the matrix
-    matrix = []
-    for i in range(len(data)):
-        row = []
-        for j in range(len(data)):
-            # Find covariance
-            prod = [(data[i][x] - data_mvs[i]['mean']) * (data[j][x] - \
-                                    data_mvs[j]['mean']) for x in range(len(data[i]))]
-            cov = sum(prod) / (len(data[i]) - 1)
-            row.append(cov)
-        matrix.append(row)
+        '''
+        # Validate user input. 
+        try:
+            if len(data) == 1 or len(data[0]) == 1:
+                raise ValueError('Input should be two dimensional list')
+        except TypeError:
+            raise ValueError('Input should be two dimensional list')
+            
+        # If labels None, create labels of list form Columni
+        if labels == None:
+            labels = ['Column' + str(n) for n in range(len(data))]
 
-    return pd.DataFrame(matrix, columns=labels, index=labels)   
+        # Populate dictionary of data lists by column name
+        self.data_set = {}
+        for i in range(len(data)):
+            self.data_set[labels[i]] = data[i]
 
+    
+    ####### Private helper functions
+    def __mvs(self, data):
+        '''
+        Calculate mean, variance, and standard deviation for an array.
 
-def correlation(data, labels=None):
-    '''
-    Calculates correlations between arrays
+        Args:
+        data - an 'array' (list) of values
 
-    input: list of arrays
-    output: variance-covariance matrix
-    '''
+        Returns:
+        dictionary with mean, variation, and standard deviation
+        '''
+        # size of array and mean
+        n = len(data)
+        mean = sum(data) / n
 
-    # Get mean and standard deviations for each array and make parallel list
-    data_mvs = []
-    size = len(data[0])
-    for item in data:
-    # Verify the size of each array is equal to the first array's size
-        assert len(item) == size, f'Array index {data.index(item)} is of different \
-                                    length.'                                               
-        data_mvs.append(mvs(item))
+        # variance and standard deviation
+        sq_differences = [(value - mean)**2 for value in data]
+        variance = sum(sq_differences) / (n - 1)
+        std = variance ** 0.5
 
-    # calculate correlations of each permutation of the two arrays, building 
-    # the matrix
-    matrix = []
-    for i in range(len(data)):
-        row = []
-        for j in range(len(data)):
-            # covariance
-            prod = [(data[i][x] - data_mvs[i]['mean']) * (data[j][x] - \
-                                    data_mvs[j]['mean']) for x in range(len(data[i]))]
-            cov = sum(prod) / (len(data[i]) - 1)
-            # correlation
-            r = cov / (data_mvs[i]['standard_deviation'] * data_mvs[j]['standard_deviation'])
-            row.append(r)
-        matrix.append(row)
-
-    return pd.DataFrame(matrix, columns=labels, index=labels)
+        # package up the results
+        statistics = {'mean': mean, 'variance': variance, 
+                                'standard_deviation': std}
+        return statistics
 
 
-def chi_2(data, labels=None):
-    '''
-    Calculate chi-square given a crosstab of two categorical values.
+    def __covar(self, colA, colB):
+        '''
+        Calculate covariance for two arrays.
 
-    Input:
-    List of two arrays of same length
+        Args:
+        ColA - an 'array' (list) of values
+        ColB - an 'array' (list) of values
 
-    Output:
-    crosstab, chi-square statistic, and p-value
-    '''
+        Returns:
+        Covariance 
+        '''
+        assert len(colA) == len(colB), 'Arrays of unequal length'
 
-    assert len(data) == 2, f'Other than two input arrays ({len(data)})'
-    assert len(data[0]) == len(data[1]), 'Inputs of different lengths!'
+        meanA = self.__mvs(colA)['mean']
+        meanB = self.__mvs(colB)['mean']
 
-    if labels == None:
-        labels = ['0', '1']
+        prod = [(colA[x] - meanA) * (colB[x] - meanB) 
+                                for x in range(len(colA))]
+        return sum(prod) / (len(colA) - 1)
+        
 
-    colA = pd.Series(data[0], name=labels[0])
-    colb = pd.Series(data[1], name=labels[1])
-    cross_data = pd.crosstab(colA, colb)
+    def __r(self, colA, colB):
+        '''
+        Calculate correlation coefficient for two arrays.
 
-    # get the row and column sums
-    row_sums = np.array(cross_data.sum(axis=1))
-    col_sums = np.array(cross_data.sum(axis=0))
-    total = sum(row_sums)
+        Args:
+        ColA - an 'array' (list) of values
+        ColB - an 'array' (list) of values
 
-    # construct expected values table
-    expected = []
-    for i in row_sums:
-        row_expected = []
-        for j in col_sums:
-            row_expected.append((i * j) / total)
-        expected.append(row_expected)
+        Returns:
+        Correlation coefficient
+        '''
+        assert len(colA) == len(colB), 'Arrays of unequal length'
 
-    # Convert cross_data and expected into one dimensional np arrays
-    expected = np.array(expected).flatten()
-    observed = np.array(cross_data).flatten()
+        covar = self.__covar(colA, colB)
 
-    # Calculate Chi_squared
-    chi_square = sum((observed - expected) ** 2 / expected)
+        stdA = self.__mvs(colA)['standard_deviation']
+        stdB = self.__mvs(colB)['standard_deviation']
+        return covar / (stdA * stdB)
+        
 
-    # And p-value (which does use scipy.stats.chi2)
-    dof = (cross_data.shape[0] - 1) * (cross_data.shape[1] - 1)
-    p_value = chi2.pdf(chi_square, dof)
-
-    return cross_data, chi_square, p_value
+    ####### Public functions
+    def get_dataframe(self):
+        '''
+        Returns the data set as a Pandas DataFrame
+        '''
+        return pd.DataFrame(self.data_set)
 
 
-# Very quick test to see if all the functions run
+    def statistics(self):
+        '''
+        Gets basic staistics for the data set. Returns as a Pandas
+        DataFrame
+        '''
+        self.stats = {}
+        for column in self.data_set.keys():
+            self.stats[column] = self.__mvs(self.data_set[column])
+
+        return pd.DataFrame(self.stats).T
+
+
+    def covariance(self):
+        '''
+        Calculates variances/covariances between arrays. Returns variance/covariance
+        matrix as Pandas Dataframe
+        '''
+        # calculate covariance/variation of each permutation of the two arrays
+        # building the matrix
+        self.cov_matrix = []
+        for i in self.data_set.values():
+            row = []
+            for j in self.data_set.values():
+                # Find covariance
+                row.append(self.__covar(i, j))
+            self.cov_matrix.append(row)
+
+        labels = self.data_set.keys()
+        return pd.DataFrame(self.cov_matrix, columns=labels, index=labels)
+
+
+    def correlation(self):
+        '''
+        Calculates correlation between arrays. Returns correlation
+        matrix as Pandas Dataframe
+        '''
+        # calculate correlations of each permutation of the two arrays, building
+        # the matrix
+        self.cor_matrix = []
+        for i in self.data_set.values():
+            row = []
+            for j in self.data_set.values():
+                # Find r (pearson's correlation)
+                row.append(self.__r(i, j))
+            self.cor_matrix.append(row)
+
+        labels = self.data_set.keys()
+        return pd.DataFrame(self.cor_matrix, columns=labels, index=labels)
+
+
+
 if __name__ == '__main__':
+    # Very quick unit tests to see if all the functions run and compare 
+    # outputs with the equivalent numpy functions
 
     from IPython.display import display
+    import numpy as np
 
+    # Some toy data
     sales = [3505, 2400, 3027, 2798, 3700, 3250, 2689]
-    customers = [3505, 2400, 3027, 2798, 3700, 3250, 2689]
+    customers = [127, 80, 105, 92, 120, 115, 93]
 
-    ctab, chi2, p = chi_2([sales, customers], labels=['sales', 'customers'])
-    display(ctab)
-    print(f'chi square = {chi2} p_value = {p}')
+    # Instantiate
+    #data = DataSet([sales, customers], ['sales', 'customers'])
+    data = DataSet([sales, customers])
 
-    print(mvs(sales))
-    display(covariance([sales, customers], labels=['sales', 'customers']))
-    display(correlation([sales, customers], labels=['sales', 'customers']))
-  
+    # Test case 1 -- Try getting dataframe
+    print('Get dataframe')
+    display(data.get_dataframe())
+
+    # Test case 2 -- And basic statistics
+    print('\nGet descriptive statistics')
+    display(data.statistics())
+
+    # Test results against numpy equivalents. Print any discrepancies.
+    passes = True
+    for col in data.stats.keys():
+        if data.stats[col]['mean'] != np.mean(data.data_set[col]):
+            passes = False
+            print(f'\'{col}\': means different\n')
+            print(f'Output: {data.stats[col]["mean"]}')
+            print(f'Actual: {np.mean(data.data_set[col])}')
+
+        if  data.stats[col]['variance'] != np.var(data.data_set[col], ddof=1):
+            passes = False
+            print(f'\'{col}\': variances different\n')
+            print(f'Output: {data.stats[col]["variance"]}')
+            print(f'Actual: {np.var(data.data_set[col], ddof=1)}')
+
+        if  data.stats[col]['standard_deviation'] != np.std(data.data_set[col], ddof=1):
+            passes = False
+            print(f'\'{col}\': standard deviations different\n')
+            print(f'Output: {data.stats[col]["standard_deviation"]}')
+            print(f'Actual: {np.std(data.data_set[col], ddof=1)}')
+    if not passes:
+        print('Some statistics cases fail')
+    else:
+        print('All statistics cases pass')
+
+    # Test case 3 -- Test covariance method. Compare with np.cov()
+    print('\nGet covariance matrix')
+    display(data.covariance())
+
+    official_value = np.cov(sales, customers)
+    if np.any(official_value != data.cov_matrix):
+        print(f'Covariance fails. Actual matrix')
+        display(pd.DataFrame(official_value))
+    else:
+        print(f'Covariance passes')
+
+    # Test case 4 -- Test correlation method. Compare with np.corrcoef()
+    print('\nGet correlation matrix')
+    display(data.correlation())
+
+    official_value = np.corrcoef(sales, customers)
+    if np.any(official_value != data.cor_matrix):
+        print(f'Correlation fails. Actual matrix')
+        display(pd.DataFrame(official_value))
+    else:
+        print(f'Correlation passes')
