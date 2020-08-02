3887912d4f2143ee5553209ce51c3f5d6748dc70
karencfisher <karen-fisher@lambdastudents.com>
2020-08-02 14:50:11 -0600

    adding ANN code

---
+++ statools/ann.py
@@ -0,0 +1,216 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+
+'''
+-------------- Activation Functions ----------------------
+Each object has two methods:
+    g: The activation function
+    dg: The partial derivative
+'''
+
+class Sigmoid():
+    def g(self, z):
+        return 1 / (1 + np.exp(z))
+    
+    def dg(self, z):
+        self.g(z) * (1 - self.g(z))
+
+    
+class Tanh():
+    def g(self, z):
+        return np.tanh(z)
+
+    def dg(self, z):
+        return 1 - np.power(np.tanh(z), 2)
+
+
+class Relu():
+    def g(self, z):
+        return max((0, z))
+
+    def dg(self, z):
+        return 0 if z < 0 else 1
+
+
+'''
+--------------------- Loss Functions ---------------------
+'''
+
+def binary_crossentropy(A, Y):
+    m = Y.shape[1]
+    logprobs = np.multiply(np.log(A), Y) + \
+        np.multiply(np.log(1 - A), (1 - Y))
+    cost = -1/m * np.sum(logprobs)
+    return np.squeeze(cost)
+
+
+def categorical_crossentropy(A, Y):
+    pass
+
+
+class Layer():
+    def __init__(self, units, activation, learning_rate, 
+                 input_shape=None, input_layer=None, 
+                 output_layer=None):
+        self.units = units
+        self.activation = activation()
+        self.learning_rate = learning_rate
+        self.input_shape = input_shape
+        self.input_layer = input_layer
+        self.output_layer = output_layer
+
+        self.m = input_shape[1]
+        self.Weights = np.random.randn(self.units, 
+                                       self.input_shape[0]) * 0.01
+        self.bias = np.zeros((self.units, 1))
+    
+    def forward(self, X):
+        self.X = X
+        self.Z = np.dot(self.Weights, self.X) + self.bias
+        self.A = self.activation.g(self.Z)
+        if self.output_layer is not None:
+            self.output_layer.forward(self.A)
+        return
+
+    def back(self, dZ):
+        if self.output_layer is None:
+            self.dZ = dZ
+        else:
+            self.dZ = np.dot(self.output_layer.prev_weights.T, dZ)
+            self.dZ *= self.activation.dg(self.Z)
+
+        self.dW = 1/self.m * np.dot(self.dZ, self.X.T)
+        self.db = 1/self.m * np.sum(self.dZ, axis=1, keepdims=True)
+
+        self.prev_weights = np.copy(self.Weights)
+        self.Weights -= self.learning_rate * self.dW
+        self.bias -= self.learning_rate * self.db
+
+        if self.input_layer is not None:
+            self.input_layer.back(self.dZ)
+        return
+        
+
+class ANN():
+    '''
+    ANN Classifier
+
+    '''
+    def __init__(self, input_shape, learning_rate, loss):
+        '''
+        Args:
+            input_shape: The dimensions of the input data to the network.
+                         N x M matrix.
+            learning_rate: The learning rate for training
+            loss: Loss function
+        '''
+        self.input_shape = input_shape
+        self.learning_rate = learning_rate
+        self.loss = loss
+        self.layers = []
+
+    def add_layer(self, units, activation):
+        '''
+        Add a layer to the neural network.
+
+        Args:
+            units: the number of neurons in the layer. Integer.
+            activation: the activation function, as one of the
+                        activation objects (Sigmoid, Tanh, Relu)
+        '''
+        if len(self.layers) > 0:
+            # Not first layer so get pointer to previous layer
+            input_layer = self.layers[-1]
+
+            # Get output shape of previous layer to provide
+            # input_shape for new layer
+            input_shape = (self.layers[-1].units, 
+                           self.input_shape[1])
+            
+            # Instantiate new layer
+            layer = Layer(units, activation, self.learning_rate,
+                               input_shape=input_shape,
+                               input_layer=input_layer)
+            
+            # Update output_layer pointer of previous layer
+            self.layers[-1].output_layer = layer
+        else:
+            # Instantiate first layer
+            layer = Layer(units, activation, self.learning_rate,
+                               input_shape=self.input_shape)
+
+        self.layers.append(layer)
+
+    def fit(self, X, y, epochs, verbose=None):
+        self.losses = []
+        self.accuracies = []
+
+        for i in range(1, epochs + 1):
+            # Feed forward getting final output
+            self.layers[0].forward(X)
+            A = self.layers[-1].A
+
+            # Map outputs
+            if self.layers[-1].units > 1:
+                y_hat = [np.argmax(A[i]) for i in range(len(A))]
+            else:
+                y_hat = (A > 0.5)
+
+            # Calculate loss and accuracy
+            loss = self.loss(A, y)
+            self.losses.append(loss)
+            accuracy = accuracy_score(y[0], y_hat[0])
+            self.accuracies.append(accuracy)
+
+            # If verbose, print info
+            if verbose is not None:
+                if i % verbose == 0:
+                    print(f'Epoch {i} Loss {loss} Accuracy {accuracy}')
+
+            # Begin back propagation
+            dZ = A - y
+            self.layers[-1].back(dZ)   
+
+    def predict(self, X):
+        # Feed forward
+        self.layers[0].forward(X)
+
+        # Get output
+        A = self.layers[-1].A
+
+        # Map to output class
+        if self.layers[-1].units > 1:
+            y_hat = [np.argmax(A[i]) for i in range(len(A))]
+        else:
+            y_hat = (A > 0.5)
+        
+        return y_hat
+
+
+
+if __name__ == '__main__':
+    from sklearn.datasets import make_blobs
+    import matplotlib.pyplot as plt
+
+    # Make some synthetic data, two classes
+    np.random.seed(0)
+    X, y = make_blobs(200, n_features=2, centers=2)
+
+    # Reshape data
+    X_train = X.T
+    y_train = y.reshape(1, -1)
+
+    clf = ANN(X_train.shape, 0.01, binary_crossentropy)
+    clf.add_layer(5, Tanh)
+    clf.add_layer(1, Sigmoid)
+
+    clf.fit(X_train, y_train, 200, verbose=10)
+
+    # plot scores over epochs
+    plt.plot(clf.losses, label='loss')
+    plt.plot(clf.accuracies, label='accuracy')
+    plt.title('Training Scores')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
--- statools/log_regression.py
+++ statools/log_regression.py
@@ -1,15 +1,14 @@
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics import accuracy_score
 
 
-class LogRegression(BaseEstimator, ClassifierMixin):
+class LogRegression():
     def __init__(self, learn_rate, epochs, verbose=10):
         self.learn_rate = learn_rate
         self.epochs = epochs
         self.verbose = verbose
 
-    def fit(self, X, Y, val_data=None):
+    def fit(self, X, Y):
         self.n = X.shape[0]
         self.m = X.shape[1]
 
@@ -19,38 +18,20 @@ class LogRegression(BaseEstimator, ClassifierMixin):
 
         # Store scores across epochs
         self.train_losses = []
-        self.train_scores = []
-        self.val_losses = []
-        self.val_scores = []
+        self.train_accuracy = []
         
         # Iterate through the epochs
         for i in range(1, self.epochs + 1):
             # Forward/Back propagation one epoch
-            loss = self.__propagate(X, Y)
-
-            # Get present training accuracy
-            accuracy = accuracy_score(Y.flatten(), 
-                                      self.predict(X).flatten())
+            loss, accuracy = self.__propagate(X, Y)
 
             # Store training scores
             self.train_losses.append(loss)
-            self.train_scores.append(accuracy)
-
-            # If validation data presented, get current scores on
-            # it too
-            if val_data is not None:
-                val_accuracy = accuracy_score(val_data[1].flatten(), 
-                                    self.predict(val_data[0]).flatten())
-                self.val_scores.append(val_accuracy)
-                val_loss = self.__loss(val_data[1], 
-                                       self.predict(val_data[0]))
-                self.val_losses.append(val_loss)
+            self.train_accuracy.append(accuracy)
 
             # If verbose > 0, print every verbose epochs
             if self.verbose > 0 and i % self.verbose == 0:
-                print(f'Epoch {i} Loss {loss} Accuracy {accuracy * 100}%')
-                if val_data is not None:
-                    print(f'Validation Accuracy {val_accuracy * 100}%')
+                print(f'Epoch {i} Loss {loss} Accuracy {accuracy}')
                     
         return self
                 
@@ -66,7 +47,13 @@ class LogRegression(BaseEstimator, ClassifierMixin):
     def __propagate(self, X, Y):
         # Forward propagate
         A = self.__sigmoid(np.dot(self.Weights.T, X) + self.bias)
+
+        # Get loss and accuracy
         cost = self.__loss(Y, A)
+        Y_prediction = np.zeros((1,self.m))
+        for i in range(self.m):
+            Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+        accuracy = accuracy_score(Y[0], Y_prediction[0])
 
         # Back propagate
         dw = 1/self.m * np.dot(X, (A - Y).T)
@@ -75,7 +62,8 @@ class LogRegression(BaseEstimator, ClassifierMixin):
         # Update parameters
         self.Weights = self.Weights - self.learn_rate * dw
         self.bias = self.bias - self.learn_rate * db
-        return cost
+
+        return cost, accuracy
 
     def __sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
@@ -99,23 +87,24 @@ if __name__ == '__main__':
     y_train = y.reshape(1, -1)
 
     # try our classifier!
-    clf = LogRegression(.01, 100, verbose=1)
+    clf = LogRegression(.01, 1000, verbose=100)
     clf.fit(X_train, y_train)
 
     # Get and print final accuracy
     y_pred = clf.predict(X_train)
-    acc = accuracy_score(y_train.flatten(), 
-                         y_pred.flatten())
-    print(f'Final accuracy {acc * 100} %')
+    acc = accuracy_score(y_train[0], 
+                         y_pred[0])
+    print(f'Final train accuracy {acc * 100} %')
 
     # plot scores over epochs
     plt.plot(clf.train_losses, label='loss')
-    plt.plot(clf.train_scores, label='accuracy')
+    plt.plot(clf.train_accuracy, label='accuracy')
     plt.title('Training Scores')
     plt.xlabel('Epochs')
     plt.legend()
     plt.show()
 
-    plt.scatter(X_train[0], X_train[1], c=y_train, alpha=.5)
-    plt.scatter(X_train[0], X_train[1], c=y_pred, alpha=.5)
+    # plot clustered data
+    plt.scatter(X_train[0], X_train[1], c=y_train[0], alpha=.5)
+    plt.scatter(X_train[0], X_train[1], c=y_pred[0], alpha=.5)
     plt.show()
